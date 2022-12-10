29f48730c944ff64c8530cb23fa20322f10245c2
Timofey <timal1@yandex.ru>
2022-12-10 12:45:27 +0300

    change method ShowNumbers2 in the ninth homework

--- HomeWork009/Program.cs
+++ HomeWork009/Program.cs
@@ -1,8 +1,8 @@
 ﻿//+ Task 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-Console.Write("Введите число N: ");
-int numberTask64 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+// Console.Write("Введите число N: ");
+// int numberTask64 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
 
 void ShowNumbers(int number)
 {
@@ -12,7 +12,7 @@ void ShowNumbers(int number)
     ShowNumbers(number - 1);
 }
 
-ShowNumbers(numberTask64);
+// ShowNumbers(numberTask64);
 
 // Task 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
@@ -30,6 +30,7 @@ int ShowNumbers2(int m, int n)
     {
         m += ShowNumbers2(m + 1, n);
     }
+    else if (m > n) m += ShowNumbers2(m - 1, n);
     return m;
 }
 
@@ -38,13 +39,13 @@ Console.WriteLine($"Сумма элементов от {m} до {n} = {ShowNumbe
 // Task 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 
-Console.Write("Введите первое неотрицательное число N: ");
-int firstNumberN = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+// Console.Write("Введите первое неотрицательное число N: ");
+// int firstNumberN = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
 
-Console.Write("Введите второе неотрицательное число M: ");
-int secondNumberM = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+// Console.Write("Введите второе неотрицательное число M: ");
+// int secondNumberM = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
 
 int AccermanFunction(int n, int m)
 {
@@ -58,4 +59,4 @@ int AccermanFunction(int n, int m)
 }
 
 
-Console.WriteLine(AccermanFunction(firstNumberN, secondNumberM));
+// Console.WriteLine(AccermanFunction(firstNumberN, secondNumberM));
